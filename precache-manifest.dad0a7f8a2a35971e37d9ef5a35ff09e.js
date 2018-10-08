2d897019acc4955b4a83c740438f3ae3eebf3d9c
Max Struever <madmaxlax@users.noreply.github.com>
2018-10-08 18:52:57 -0400

    Updates

---
+++ precache-manifest.dad0a7f8a2a35971e37d9ef5a35ff09e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3474ac909b7cdc4f6b8f",
+    "url": "/react-redux-todo/static/js/runtime~main.3474ac90.js"
+  },
+  {
+    "revision": "756ae98743e701869ac4",
+    "url": "/react-redux-todo/static/js/main.756ae987.chunk.js"
+  },
+  {
+    "revision": "23aee64bce0b932a62b5",
+    "url": "/react-redux-todo/static/js/1.23aee64b.chunk.js"
+  },
+  {
+    "revision": "756ae98743e701869ac4",
+    "url": "/react-redux-todo/static/css/main.8e68d87b.chunk.css"
+  },
+  {
+    "revision": "98ae2f1c2c335330fd86b6d1b20e14e3",
+    "url": "/react-redux-todo/index.html"
+  }
+];
