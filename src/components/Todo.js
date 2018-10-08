eb3691056bba4adbbfccc94bcbc4e46158cce185
Max Struever <madmaxlax@users.noreply.github.com>
2018-10-08 18:45:13 -0400

    orig plus check boxes

---
+++ src/components/Todo.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Todo = ({ onClick, completed, text }) => (
+  <li
+    onClick={onClick}
+    style={{
+      textDecoration: completed ? 'line-through' : 'none'
+    }}
+  ><input type="checkbox" checked={completed} />
+    {text}
+  </li>
+)
+
+Todo.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  completed: PropTypes.bool.isRequired,
+  text: PropTypes.string.isRequired
+}
+
+export default Todo
